6bcae8475e408860569aa21fcfd192ac30de7fa0
Sagar Ghat <sagarghat2004@gmail.com>
2025-10-30 17:06:48 +0530

    Updated the server.js

--- server.js
+++ server.js
@@ -31,6 +31,10 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: 'Server error' });
 });
 
+app.use((req, res) => {
+  res.status(404).json({ message: 'Route not found' });
+});
+
 // START SERVER
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
